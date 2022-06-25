016197b80bdda08f4f118c973ae1c5ba4c54f370
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-06-25 16:18:34 -0400

    add 3d animation to WeatherIcon

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,4 +1,5 @@
 import { Transition } from "@mantine/core"
+import { useMouse, useViewportSize } from "@mantine/hooks"
 import { city } from "~/constants/propTypes"
 import weatherCodeToIcon from "~/constants/weatherCodeToIcon"
 import useWeather from "~/hooks/useWeather"
@@ -7,21 +8,34 @@ const WeatherIcon = ({ city }) => {
   const { weather, isLagging } = useWeather(city.id)
   const icon = weatherCodeToIcon[weather?.weather[0]?.icon]
 
+  const { width, height } = useViewportSize()
+  const { x, y } = useMouse()
+  const center = { x: width / 2, y: height / 2 }
+  const rotateY = (x - center.x) / (center.x / 20)
+  const rotateX = (y - center.y) / (center.y / 20)
+
   return (
-    <div>
-      <Transition
-        transition="scale"
-        duration={400}
-        timingFunction="ease"
-        mounted={!isLagging || !icon}
-      >
-        {(styles) => (
-          <div style={{ ...styles }}>
-            <img src={`weather/${icon}.png`} width={260} alt={icon} />
-          </div>
-        )}
-      </Transition>
-    </div>
+    <Transition
+      transition="scale"
+      duration={400}
+      timingFunction="ease"
+      mounted={!isLagging || !icon}
+    >
+      {(styles) => (
+        <div style={{ ...styles, perspective: 500 }}>
+          <img
+            src={`weather/${icon}.png`}
+            width={260}
+            alt={icon}
+            style={{
+              transform: `
+                rotateX(${rotateX}deg) 
+                rotateY(${rotateY}deg)`,
+            }}
+          />
+        </div>
+      )}
+    </Transition>
   )
 }
 
