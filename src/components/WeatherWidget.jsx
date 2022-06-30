3583f9e290d48b3506620abd173d99301ae8a9f4
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-06-30 19:19:29 -0400

    revert last commit :)

--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -10,7 +10,7 @@ const WeatherWidget = () => {
   return (
     <WidgetPaper sx={{ width: "400px", height: "500px" }}>
       <Stack sx={{ height: "100%" }}>
-        <Group >
+        <Group spacing="xs">
           <CityInput />
           <TempUnitSwitch />
         </Group>
